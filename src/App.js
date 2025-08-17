cc41c6befac7447ddae88f938bd98e5d2eb06848
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-17 21:14:29 +0500

    Resetup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import Header from "./Header";
-import LoginButton from "./LoginButton";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Header />
-      <LoginButton />
-    </div>
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
   );
 }
+
+export default App;
