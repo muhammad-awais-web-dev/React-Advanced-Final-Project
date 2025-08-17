b87a88f02be7346ab1e352f17a26083bddf2e431
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-17 21:49:54 +0500

    Landing Section done

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,6 +15,14 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={8}>
+      <VStack spacing={3}>
+        <Avatar name="pete" src="https://i.pravatar.cc/150?img=7" size="2x1" />
+        <Heading size="md">{greeting}</Heading>
+      </VStack>
+        <Heading size="2xl">{bio1}</Heading>
+        <Heading size="2xl">{bio2}</Heading>
+    </VStack>
 
   </FullScreenSection>
 );
